5bcefbd6759816c0c1c7f146534eab17cd8ace66
Andrew Hunsaker <ahuns2@byu.edu>
2024-02-02 16:56:43 -0700

    We finished

--- Program.cs
+++ Program.cs
@@ -1,84 +1,102 @@
 ﻿//Grant and Ava
 using Mission04_Team0310;
 using System.ComponentModel.Design;
-//Create a new object 
-supportingclass sc = new supportingclass();
 
-//Welcome the user to the game. Initialize variables
-Console.WriteLine("Hello Players! Welcome to our game! ");
-static int currentPlayer = 0;
-static bool gameOver = false;
-static int winner = 0;
-
-//Create the game board array
-static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-//Do loop to play the game until someone wins or a draw
-do
+class Driver
 {
-    sc.PrintBoard(board);
-    GetUserChoice();
-    CheckForGameEnd();
-
-} while (gameOver == false);
-
-//This method gets the Users choice.
-static void GetUserChoice()
-{
-    Console.WriteLine('Where would you like to play? ')
-    int choice = int.Parse(Console.ReadLine());
-
-//Checks to make sure the input is valid
-    if (board[choice - 1] != 'X' && board[choice - 1] != 'O' && board[choice - 1] >= '1' && board[choice - 1] <= '9') ;
+    public static void Main()
     {
-        //Recording the players choice depending on which player is going
-        if (currentPlayer == 0)
+        //Create a new object 
+        supportingclass sc = new supportingclass();
+
+        //Welcome the user to the game. Initialize variables
+        Console.WriteLine("Hello Players! Welcome to our game! ");
+        int currentPlayer = 0;
+        bool gameOver = false;
+
+        //Create the game board array
+        char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        //Do loop to play the game until someone wins or a draw
+        while (gameOver == false)
         {
-            board[choice - 1] = 'O';
-            currentPlayer = 1;
+            sc.PrintBoard(board);
+            GetUserChoice(currentPlayer, board);
+            gameOver = CheckForGameEnd(currentPlayer, board);
+            currentPlayer += 1;
+        }
+;
+    }
+
+
+    //This method gets the Users choice.
+    static void GetUserChoice(int currentPlayer, char[] board)
+    {
+        if (currentPlayer % 2 == 0)
+        {
+            Console.WriteLine("O's turn! Where would you like to play?");
         }
         else
         {
-            board[choice - 1] = 'X';
-            currentPlayer = 0;
+            Console.WriteLine("X's turn! Where would you like to play?");
         }
- 
-    }
 
-    //Error message
-    else
-    {
-        Console.WriteLine("Invalid input, try again");
-        GetUserChoice();
-    }
-}
+        int choice;
 
-//This method checks to see if the game has ended
-static void CheckForGameEnd()
-{
-    gameOver = sc.CheckForWinner(board);
-
-    if (gameOver == true)
-    {
-        sc.PrintBoard(board);
-
-        if (currentPlayer == 0)
+        // Keep prompting the user until a valid input is provided
+        while (true)
         {
-            int winner = 1;
-            Console.WriteLine($"Player {winner} wins");
-        }
-        else
-        {
-            int winner = 0;
-            Console.WriteLine($"Player {winner} wins");
+            // Try to parse the input
+            if (int.TryParse(Console.ReadLine(), out choice) && choice >= 1 && choice <= 9)
+            {
+                // Check if the selected square is not already occupied
+                if (board[choice - 1] != 'X' && board[choice - 1] != 'O')
+                {
+                    // Record the player's choice depending on which player is going
+                    board[choice - 1] = (currentPlayer % 2 == 0) ? 'O' : 'X';
+                    break; // Break out of the loop when a valid input is provided
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. The chosen square is already occupied. Please try again.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter an integer between 1 and 9.");
+            }
         }
     }
 
-    //Checks for a draw
-    else if (Array.TrueForAll(board, c => c == 'X' || c == 'O'))
+    //This method checks to see if the game has ended
+    static bool CheckForGameEnd(int currentPlayer, char[] board)
     {
-        sc.PrintBoard(board);
-        Console.WriteLine("It's a draw!");
-        gameOver = true;
+        supportingclass sc = new supportingclass();
+
+        bool gameOver = sc.CheckForWinner(board);
+
+        if (gameOver == true)
+        {
+            sc.PrintBoard(board);
+
+            if (currentPlayer % 2 == 0)
+            {
+                Console.WriteLine($"\nO's win!");
+            }
+            else
+            {
+                Console.WriteLine($"\nX's win!");
+            }
+        }
+
+        //Checks for a draw
+        else if (Array.TrueForAll(board, c => c == 'X' || c == 'O'))
+        {
+            sc.PrintBoard(board);
+            Console.WriteLine("\nIt's a draw!");
+            gameOver = true;
+        }
+
+        return gameOver;
     }
 }
--- supportingclass.cs
+++ supportingclass.cs
@@ -7,6 +7,40 @@ using System.Threading.Tasks;
 namespace Mission04_Team0310
 {
     internal class supportingclass
-    {
+    { 
+        // Method that receives the board array and prints the board based on the information passed to it
+        public void PrintBoard(char[] board)
+        {
+            Console.WriteLine($"\n {board[0]} | {board[1]} | {board[2]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[3]} | {board[4]} | {board[5]} ");
+            Console.WriteLine("---|---|---");
+            Console.WriteLine($" {board[6]} | {board[7]} | {board[8]} \n");
+        }
+
+        // Method to check for a winner
+        public bool CheckForWinner(char[] board)
+        {
+            // Check rows, columns, and diagonals for a winner
+            for (int i = 0; i < 3; i++)
+            {
+                // Check rows
+                if (board[i * 3] == board[i * 3 + 1] && board[i * 3 + 1] == board[i * 3 + 2])
+                    return true;
+
+                // Check columns
+                if (board[i] == board[i + 3] && board[i + 3] == board[i + 6])
+                    return true;
+            }
+
+            // Check diagonals
+            if (board[0] == board[4] && board[4] == board[8])
+                return true;
+
+            if (board[2] == board[4] && board[4] == board[6])
+                return true;
+
+            return false;
+        }
     }
 }
